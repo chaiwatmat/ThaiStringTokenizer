28d7eb26e4e8122b76940a4652c90714cf98f640
Chaiwat Matarak <chaiwat.mat@gmail.com>
2019-11-19 20:50:27 +0700

    add more unit test

--- ThaiStringTokenizerTest/UnitTest3.cs
+++ ThaiStringTokenizerTest/UnitTest3.cs
@@ -56,5 +56,95 @@ namespace ThaiStringTokenizerTest
                 index++;
             });
         }
+
+        [Fact]
+        public void TestSplit_ThaiWithEnglish()
+        {
+            ThaiTokenizer tokenizer = new ThaiTokenizer();
+            string text = "ทดสอบไทยคำ อังกฤษคำ Test Thai language with English";
+            var result = tokenizer.Split(text);
+
+            var expected = new List<string>
+            {
+                "ทดสอบ",
+                "ไทย",
+                "คำ",
+                "อังกฤษ",
+                "คำ",
+                "Test",
+                "Thai",
+                "language",
+                "with",
+                "English"
+            };
+
+            Assert.Equal(expected.Count, result.Count);
+
+            var index = 0;
+            result.ForEach(x =>
+            {
+                Assert.Equal(expected[index], x);
+                index++;
+            });
+        }
+
+        [Fact]
+        public void TestSplitKeepSpace_ThaiWithEnglish()
+        {
+            ThaiTokenizer tokenizer = new ThaiTokenizer(removeSpace: false);
+            string text = "ทดสอบไทยคำ อังกฤษคำ Test Thai language with English";
+            var result = tokenizer.Split(text);
+
+            var expected = new List<string>
+            {
+                "ทดสอบ",
+                "ไทย",
+                "คำ",
+                " ",
+                "อังกฤษ",
+                "คำ",
+                " ",
+                "Test",
+                " ",
+                "Thai",
+                " ",
+                "language",
+                " ",
+                "with",
+                " ",
+                "English"
+            };
+
+            Assert.Equal(expected.Count, result.Count);
+
+            var index = 0;
+            result.ForEach(x =>
+            {
+                Assert.Equal(expected[index], x);
+                index++;
+            });
+        }
+
+        [Fact]
+        public void TestSubThaiString_ThaiWithEnglish()
+        {
+            ThaiTokenizer tokenizer = new ThaiTokenizer(removeSpace: false);
+            string text = "ทดสอบไทยคำ อังกฤษคำ Test Thai language with English";
+            var result = tokenizer.SubThaiString(text, 50);
+
+            var expected = new List<string>
+            {
+                "ทดสอบไทยคำ อังกฤษคำ Test Thai language with English"
+            };
+
+            Assert.Equal(expected.Count, result.Count);
+
+            var index = 0;
+            result.ForEach(x =>
+            {
+                Assert.Equal(expected[index], x);
+                index++;
+            });
+        }
     }
 }
