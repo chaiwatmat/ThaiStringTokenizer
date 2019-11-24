f8a0b81735a705bf849a43eb9a5285be38cc5d98
Chaiwat Matarak <chaiwat.mat@gmail.com>
2019-11-24 09:07:05 +0700

    add script

---
+++ send_codecov.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dotnet build
+
+dotnet test ThaiStringTokenizerTest \
+    /p:CollectCoverage=true \
+    /p:CoverletOutputFormat=\"opencover\" \
+    /p:Threshold=80 \
+    /p:ThresholdType=branch
+
+./ThaiStringTokenizerTest/codecov -f "ThaiStringTokenizerTest/coverage.opencover.xml" -t $CODECOV_TOKEN
