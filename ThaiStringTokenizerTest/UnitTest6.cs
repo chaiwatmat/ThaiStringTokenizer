57ff0ffb936a87bf4c33732c103ff9f58c2d29dd
Chaiwat Matarak <chaiwat.mat@gmail.com>
2019-11-26 22:34:36 +0700

    add BasicLatinCharacter class

---
+++ ThaiStringTokenizerTest/UnitTest6.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThaiStringTokenizer;
+using Xunit;
+
+namespace ThaiStringTokenizerTest
+{
+    public class UnitTest6
+    {
+        [Fact]
+        public void TestCountBasicLatinCharacters()
+        {
+            var characters = BasicLatinCharacter.Characters;
+            var expectedCharactersExcludeSpace = 94;
+
+            Assert.Equal(expectedCharactersExcludeSpace, characters.Count);
+        }
+    }
+}
