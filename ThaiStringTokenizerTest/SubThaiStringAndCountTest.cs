6a76c338b1552ca826922ed16608bdde890d9ea0
Chaiwat Matarak <chaiwat.mat@gmail.com>
2019-12-03 20:56:15 +0700

    add test case

--- ThaiStringTokenizerTest/SubThaiStringAndCountTest.cs
+++ ThaiStringTokenizerTest/SubThaiStringAndCountTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using ThaiStringTokenizer;
+using ThaiStringTokenizer.Models;
 using Xunit;
 
 namespace ThaiStringTokenizerTest
@@ -21,6 +22,36 @@ namespace ThaiStringTokenizerTest
 
             Assert.Equal(expectedList, results.Count);
             Assert.Equal(expectedCount, results[0].Countable);
+        }
+
+        [Fact]
+        public void TestSubThaiStringAndCount_WithLimitLength()
+        {
+            var input = "ถ้าหากรักนี้ ไม่บอกไม่พูดไม่กล่าว แล้วเขาจะรู้ว่ารักหรือเปล่า";
+
+            var tokenizer = new ThaiTokenizer(removeSpace: false);
+            var results = tokenizer.SubThaiStringAndCount(input, 24);
+
+            var expectedResults = new List<ThaiStringResponse>
+            {
+                new ThaiStringResponse{ Words = "ถ้าหากรักนี้ ไม่บอกไม่พูดไม่กล่าว", Countable = 24},
+                new ThaiStringResponse{ Words = " แล้วเขาจะรู้ว่ารักหรือเปล่า", Countable = 21}
+            };
+
+            Assert.Equal(expectedResults.Count, results.Count);
+
+            for (var i = 0; i < expectedResults.Count; i++)
+            {
+                var expectedWord = expectedResults[i].Words;
+                var expecptedCount = expectedResults[i].Countable;
+
+                var actualWord = results[i].Words;
+                var actualCount = results[i].Countable;
+
+                Assert.Equal(expectedWord, actualWord);
+                Assert.Equal(expecptedCount, actualCount);
+
+            }
         }
     }
 }
