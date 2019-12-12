fd85068edddebd2dba0e942aa6eb9632be92ea30
Chaiwat Matarak <chaiwat.mat@gmail.com>
2019-12-12 18:55:42 +0700

    add uncountable chars for response

--- ThaiStringTokenizer/Models/ThaiStringResponse.cs
+++ ThaiStringTokenizer/Models/ThaiStringResponse.cs
@@ -7,5 +7,6 @@ namespace ThaiStringTokenizer.Models
     {
         public string Words { get; set; }
         public int Countable => Words.ToCharArray().ToList().Count(x => !ThaiUnicodeCharacter.UncountForPrint.Contains(x));
+        public int Uncountable => Words.ToCharArray().ToList().Count(x => ThaiUnicodeCharacter.UncountForPrint.Contains(x));
     }
 }
