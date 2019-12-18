3ab43bf97a04d059a53c50694d207a0c3ca978e1
Chaiwat Matarak <chaiwat.mat@gmail.com>
2019-12-18 23:10:44 +0700

    update test

---
+++ ThaiStringTokenizerTest/ThaiNameTest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThaiStringTokenizer;
+using ThaiStringTokenizer.Characters;
+using Xunit;
+
+namespace ThaiStringTokenizerTest
+{
+    public class ThaiNameTest
+    {
+        [Fact]
+        public void SampleTest1()
+        {
+            var input = "โชติกานต์";
+            var expected = new List<string> { "โชติกา", "นต์" };
+
+            var tokenizer = new ThaiTokenizer(removeSpace: false);
+            var results = tokenizer.Split(input);
+
+            var index = 0;
+            results.ForEach(x =>
+            {
+                Assert.Equal(expected[index], x);
+                index++;
+            });
+        }
+    }
+}
