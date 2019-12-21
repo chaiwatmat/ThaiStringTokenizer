84a34aced59b5391ab348b8223b84d7e0329b8da
Chaiwat Matarak <chaiwat.mat@gmail.com>
2019-12-21 11:18:39 +0700

    update test

--- ThaiStringTokenizerTest/ThaiNameTest.cs
+++ ThaiStringTokenizerTest/ThaiNameTest.cs
@@ -25,5 +25,65 @@ namespace ThaiStringTokenizerTest
                 index++;
             });
         }
+
+        [Fact]
+        public void SampleTest2()
+        {
+            var input = "กุญจ์สิริลัญฉกร พจชรดลญา ณ สงขลา";
+            var expected = new List<string> { "กุญจ์", "สิริลัญ", "ฉกร", " ", "พจ", "ชร", "ดล", "ญา", " ", "ณ สงขลา" };
+
+            var tokenizer = new ThaiTokenizer(removeSpace: false);
+            var results = tokenizer.Split(input);
+
+            var index = 0;
+            results.ForEach(x =>
+            {
+                Console.WriteLine(x);
+                Assert.Equal(expected[index], x);
+                index++;
+            });
+
+            Console.WriteLine("==============");
+        }
+
+        [Fact]
+        public void SampleTest3()
+        {
+            var input = "กุญจ์สิริลัญฉกร พจชรดลญา ณ นคร";
+            var expected = new List<string> { "กุญจ์", "สิริลัญ", "ฉกร", " ", "พจ", "ชร", "ดล", "ญา", " ", "ณ นคร" };
+
+            var tokenizer = new ThaiTokenizer(removeSpace: false);
+            var results = tokenizer.Split(input);
+
+            var index = 0;
+            results.ForEach(x =>
+            {
+                Console.WriteLine(x);
+                Assert.Equal(expected[index], x);
+                index++;
+            });
+
+            Console.WriteLine("==============");
+        }
+
+        [Fact]
+        public void SampleTest4()
+        {
+            var input = "กุญจ์สิริลัญฉกร พจชรดลญา ณ นครศรีธรรมราช";
+            var expected = new List<string> { "กุญจ์", "สิริลัญ", "ฉกร", " ", "พจ", "ชร", "ดล", "ญา", " ", "ณ นคร", "ศรีธรรม", "ราช" };
+
+            var tokenizer = new ThaiTokenizer(removeSpace: false);
+            var results = tokenizer.Split(input);
+
+            var index = 0;
+            results.ForEach(x =>
+            {
+                Console.WriteLine(x);
+                Assert.Equal(expected[index], x);
+                index++;
+            });
+
+            Console.WriteLine("==============");
+        }
     }
 }
