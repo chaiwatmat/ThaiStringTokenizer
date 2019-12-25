a1bcdabe81aaeecbde87a962ebee5799f1c68a73
Chaiwat Matarak <chaiwat.mat@gmail.com>
2019-12-25 22:23:57 +0700

    refactor: fix code style

--- ThaiStringTokenizer/CharacterHandler.cs
+++ ThaiStringTokenizer/CharacterHandler.cs
@@ -6,10 +6,18 @@ namespace ThaiStringTokenizer
     public abstract class CharacterHandler
     {
         public Dictionary<char, List<string>> Dictionary { get; set; } = new Dictionary<char, List<string>>();
+
         public bool RemoveSpace { get; set; }
+
         public bool ShortWordFirst { get; set; }
 
         public string[] Words { get; set; }
+
+        public bool IsEnglishCharacter(char charNumber) => BasicLatinCharacter.Alphabets.Contains(charNumber);
+
+        public bool IsNumberCharacter(char charNumber) => BasicLatinCharacter.Digits.Contains(charNumber);
+
+        public bool IsThaiCharacter(char charNumber) => ThaiUnicodeCharacter.Characters.Contains(charNumber);
 
         public void HandleConsonantOrVowel(List<string> outputList, char[] characters, ref string tmpString, ref int i)
         {
@@ -92,12 +100,5 @@ namespace ThaiStringTokenizer
             outputList.Add(tmpString);
             tmpString = "";
         }
-
-        public bool IsEnglishCharacter(char charNumber) => BasicLatinCharacter.Alphabets.Contains(charNumber);
-
-        public bool IsNumberCharacter(char charNumber) => BasicLatinCharacter.Digits.Contains(charNumber);
-
-        public bool IsThaiCharacter(char charNumber) => ThaiUnicodeCharacter.Characters.Contains(charNumber);
-
     }
 }
