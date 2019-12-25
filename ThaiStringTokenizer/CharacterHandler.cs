fdc625b1b86a84080eb7d618770136dfdefa27f4
Chaiwat Matarak <chaiwat.mat@gmail.com>
2019-12-25 22:18:45 +0700

    refactor: pull up some methods

--- ThaiStringTokenizer/CharacterHandler.cs
+++ ThaiStringTokenizer/CharacterHandler.cs
@@ -11,6 +11,88 @@ namespace ThaiStringTokenizer
 
         public string[] Words { get; set; }
 
+        public void HandleConsonantOrVowel(List<string> outputList, char[] characters, ref string tmpString, ref int i)
+        {
+            tmpString += characters[i].ToString();
+
+            bool isFound = false;
+            string moretmp = tmpString;
+
+            for (int j = i + 1; j < characters.Length; j++)
+            {
+                moretmp += characters[j].ToString();
+                var firstCharacter = moretmp[0];
+
+                if (!Dictionary.ContainsKey(firstCharacter)) { continue; }
+
+                foreach (var word in Dictionary[firstCharacter])
+                {
+                    if (word == moretmp)
+                    {
+                        tmpString = moretmp;
+                        i = j;
+                        isFound = true;
+                        break;
+                    }
+                }
+
+                if (ShortWordFirst)
+                {
+                    break;
+                }
+            }
+
+            if (isFound)
+            {
+                outputList.Add(tmpString);
+            }
+            else
+            {
+                var lastOutputIndex = outputList.Count - 1;
+
+                outputList[lastOutputIndex] += tmpString;
+            }
+            tmpString = "";
+        }
+
+        public void HandleEnglishCharacter(List<string> outputList, char[] characters, ref string tmpString, ref int i)
+        {
+            tmpString += characters[i].ToString();
+            for (int j = i + 1; j < characters.Length; j++)
+            {
+                if (IsEnglishCharacter(characters[j]))
+                {
+                    tmpString += characters[j];
+                    i = j;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            outputList.Add(tmpString);
+            tmpString = "";
+        }
+
+        public void HandleNumberCharacter(List<string> outputList, char[] characters, ref string tmpString, ref int i)
+        {
+            tmpString += characters[i].ToString();
+            for (int j = i + 1; j < characters.Length; j++)
+            {
+                if (IsNumberCharacter(characters[j]))
+                {
+                    tmpString += characters[j];
+                    i = j;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            outputList.Add(tmpString);
+            tmpString = "";
+        }
+
         public bool IsEnglishCharacter(char charNumber) => BasicLatinCharacter.Alphabets.Contains(charNumber);
 
         public bool IsNumberCharacter(char charNumber) => BasicLatinCharacter.Digits.Contains(charNumber);
